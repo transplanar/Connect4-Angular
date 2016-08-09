c1335e1283156b4ba4172df7c2c3fca97c057014
transplanar <transplanar@gmail.com>
2016-08-09 19:27:40 -0400

    condense hor/vert match to single function

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -5,6 +5,8 @@
     var gridX = 6;
     var gridY = 7;
     
+    const NUM_IN_A_ROW = 4;
+    
     $scope.gridArr = new Array();
     
     $scope.generateGrid = function(){
@@ -50,138 +52,82 @@
       }
     };
     
-    var matchesHorizontal = function(spaceArr){      
-      var inRow = _.groupBy(spaceArr, 'y');
-      var result = false;
-      
-      _.each(inRow, function(row){ 
-        if(row.length == 4){
-          result = true;;
+    var getNumConsecutive = function(arr){
+      console.log('test consecutive on',arr);
+      if(arr.length < 2){
+        return 1;
+      }
+
+      var count = 1;
+      var highestCount = 1;
+
+      arr.sort(function(a,b){return a-b});
+
+      for(var i = arr.length-1; i > 0; i--){
+        if( Math.abs(arr[i] - arr[i-1]) == 1 ) {
+          count++;
+          if(count > highestCount){
+            highestCount = count;
+          }
+        }else{
+          count=1;
+        }
+      }
+
+      return highestCount;
+    }
+    
+    var testMatchAxis = function(arr, axis){
+      var inRow = _.groupBy(arr, axis);
+      var result = false;           
+
+      var vals = [];
+
+      _.each(inRow, function(row){
+        _.each(row, function(space){
+          vals.push(space[axis]);
+        })
+
+        var num = getNumConsecutive(vals);
+        console.log('consec',num);
+          
+        
+        if(num == NUM_IN_A_ROW){
+          result = true;
         }
       });
-      
+
       return result;
     }
     
     var matchesVertical = function(spaceArr){      
-      var inRow = _.groupBy(spaceArr, 'x');
+      var inCol = _.groupBy(spaceArr, 'x');
       var result = false;
 
-      _.each(inRow, function(row){ 
-        if(row.length == 4){
+      _.each(inCol, function(col){ 
+        if(col.length == 4){
           result = true;;
         }
       });
 
       return result;
     }
-    
-    var matchesDiagonal = function(spaceArr){
-      var matches = 1;
-      _.reduce(spaceArr, function(memo, space){
-        if(memo != null){
-          if(space.x == memo.x){
-            matches++;
-          }
-        }
-
-        return space;
-      });
-
-      return matches == 4;
-    }
-    
-    
+        
     var checkMatches = function(){      
-//      console.log('checking matches');
       var flatGrid = _.flatten($scope.gridArr);
       
       var p1Spaces = _.filter(flatGrid, function(space){ return space.owner == 1; })
-      var p2Spaces = _.filter(flatGrid.gridArr, function(space){ return space.owner == 2; })
-      
-//      console.log('p1 spaces',p1Spaces);
-//      console.log('p2 spaces',p2Spaces);
-      
+      var p2Spaces = _.filter(flatGrid, function(space){ return space.owner == 2; })
+            
       var matches = 1;
       
-      if(matchesHorizontal(p1Spaces) || matchesVertical(p1Spaces)
-         || matchesHorizontal(p2Spaces) || matchesHorizontal(p2Spaces)){
+      if(testMatchAxis(p1Spaces,'x') || testMatchAxis(p1Spaces,'y')){
          return true;
-      }
+      };
       
-//      matchesHorizontal(p2Spaces);
-//      matchesVertical(p2Spaces);
-//      matchesDiagonal();
-      
-//      Horizontal
-//      _.reduce(p1Spaces, function(memo, space){
-//        if(memo != null){
-//          if(space.y == memo.y){
-//            matches++;
-//          }
-//        }
-//
-//        return space;
-//      });
-//      
-//      
-//      
-//      console.log('matches',matches);
-//      
-//      if (matches == 4){
-//        return true;
-//      }
-      
-//      console.log('player 1 spaces ' + p1Spaces);
-//      console.log('player 2 spaces ' + p2Spaces);
-      
-//      _.each($scope.gridArr, function(row){
-//        _.each(row, function(space){
-//          if(space.owner)
-//        })
-//      });
-      
-      
-//      for(var i = 0; i < gridX; i++){
-//        for(var j = 0; j < gridY; j++){
-//          if($scope.gridArr[i][j].owner)
-//        }
-//      }
-      
-//      Template
-//      for(var i = 0; i < gridX; i++){
-//        for(var j = 0; j < gridY; j++){
-//          
-//        }
-//      }
-
-      
-      
-//      
-//      for(var p = 1; p <= 2; p++ ){
-//        matches = 0;
-//        
-//        for(var i = 0; i < gridX; i++){
-//          matches = 0;
-//          for(var j = 0; j < gridY; j++){
-////            if($scope.gridArr[i][j].owner != null){
-////              console.log('testing ' , $scope.gridArr[i][j]);
-////            }
-//            
-//            
-//            
-//            
-//            if($scope.gridArr[i][j].owner == p){
-//              matches++;
-//              
-//              console.log('matches in a row', matches, 'for player', p);
-//              
-//              if(matches == 4){
-//                return true;
-//              }
-//            }
-//          }
-//        }      
+//      if(matchesHorizontal(p1Spaces) || matchesVertical(p1Spaces)
+//         || matchesHorizontal(p2Spaces) || matchesVertical(p2Spaces)){
+//         return true;
 //      }
       
       return false;
@@ -198,6 +144,7 @@
         }else{
           console.log('Player ',$scope.currentPlayer,'wins!');
         }
+        
 //        $scope.currentPlayer = togglePlayer();
       }
     };    
